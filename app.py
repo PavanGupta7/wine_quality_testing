adca7285bd4a8805c727b4d4749d505c145a9586
PavanGupta7 <pawang318@gmail.com>
2024-05-11 21:25:20 +0530

    Created app.py using predict and API response method.Next step is to test API Using POSTMAN API

---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, request , jsonify
+import os
+import yaml
+import joblib
+import numpy as np
+import pandas as pd
+
+params_path = "params.yaml"
+webapp_root = "webapp"
+
+static_dir = os.path.join(webapp_root,"static")
+template_dir = os.path.join(webapp_root, "templates")
+
+app = Flask(
+    __name__, 
+    static_folder = static_dir,
+    template_folder = template_dir
+)
+
+def read_params(config_path):
+    with open(config_path) as yaml_f:
+        config = yaml.safe_load(yaml_f)
+    return config
+
+#predict methods for prediction
+def predict(data):
+    config = read_params(params_path)
+    model_dir = config["webapp_model_dir"]
+    model = joblib.load(model_dir)
+    y_pred = model.predict(data)
+    print(y_pred)
+    return y_pred[0]
+
+def api_response():
+    try:
+        data = np.array([list(request.json.value())])
+        print(data)
+        response = predict(data)
+        response = {"response":response}
+        return response
+    except Exception as e:
+        print(e)
+        error = {"error": "Something went Wrong!"}
+        return error
+
+
+#creating index root
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == "POST":
+        try:
+            if request.form:
+                data = dict(request.form)   #reuquesting all the variables  and storing it in float format
+                data = [list(map(float, data))]
+                response = predict(data)
+                return render_template("index.html", response = response)  
+
+
+            elif request.json: 
+                #if we want to query it in API that will be stored in json format
+                response= api_response(request)
+                return jsonify(response)
+
+
+        except Exception as e:
+            print(e)
+            error = {"error": "Something went Wrong!"}
+            return render_template("404.html", error = error)
+        pass
+    else:
+        return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", debug = True, port= 5000)
